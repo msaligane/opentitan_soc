7ac021a60c3c1f2cb282d7be4a633df7a7101a50
Wenbo Duan <wbduan@vlsipool-j08.eecs.umich.edu>
2021-04-27 07:45:07 -0400

    add ff register and rv_core_ibex modules from opentitan

--- ibex/rtl/top_pkg.sv
+++
@@ -1,9 +0,0 @@
-package top_pkg;
-  localparam TL_AW=32;
-  localparam TL_DW=32;
-  localparam TL_AIW=8;
-  localparam TL_DIW=1;
-  localparam TL_DUW=4;
-  localparam TL_DBW=(TL_DW>>3);
-  localparam TL_SZW=$clog2($clog2(TL_DBW)+1);
-endpackage
